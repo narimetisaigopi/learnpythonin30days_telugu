487e12cd3301783716dddb2ec47d1bc85aed12bb
saigopi <narimetisaigopi@gmail.com>
2020-10-14 15:59:57 +0530

    Day 01 added

---
+++ comments_day01.py
@@ -0,0 +1,14 @@
+#This is simple comment
+print("Hello World ") #This is simple comment
+#print("Hello World by sai gopi tech telugu") 
+
+# multiline comments
+# this is programming of python in telugu. 
+# This tutorials are providing by sai gopu tech telugu.
+
+# multiline comments ( strings way)
+
+'''
+this is programming of python in telugu. 
+This tutorials are providing by sai gopu tech telugu.
+'''
---
+++ indentation_day01.py
@@ -0,0 +1,12 @@
+# good format
+if 4> 2:
+    print(" four is greater than 2")
+
+# good format
+if 6> 2:
+    print(" six is greater than 2")
+
+
+if 8> 2:
+  print(" eight is greater than 2")
+    #print(" eight is bigger") # bad format
