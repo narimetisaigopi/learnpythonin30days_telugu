ba1978c1e39d7935939940db3166e17fe15de740
saigopi <narimetisaigopi@gmail.com>
2020-10-17 01:11:50 +0530

    Day 4

---
+++ data_types_number_day4.py
@@ -0,0 +1,44 @@
+x = 12
+y = 12.00
+z = 1j
+
+
+# int
+x = 12333
+x = -12333
+x = -12339999999992132392392392932932939239
+
+#print(type(x))
+
+
+# float (Float point number)
+
+x = 12.80099291291939139193193
+y = -14322.89
+z = 121121212121212.1
+
+#print(type(z))
+
+x = 2e3
+x = -34.4e3
+
+# complex
+
+x = 3+5j
+
+#print(type(x))
+
+#***************** we can convert complex data type to number **********
+
+age = 25
+percentate = 95.67
+z = 1j
+
+x = float(age)
+print(x)
+
+x = int(percentate)
+print(x)
+
+x = complex(percentate)
+print(x)
