53c99d48f5c98c47275ff60364f9156ee344a1c0
saigopi <narimetisaigopi@gmail.com>
2020-10-22 06:50:50 +0530

    Day 03

--- data_types_day03.py
+++ data_types_day03.py
@@ -1,15 +1,11 @@
-age = '25' # single or double
-
-print(type(age))
-
 x = "String world " # string
 x = 25 #int
 x = 25.04 #float
 x = 1j # complex
-x = ['python','Dart','Java','Dart'] #List ( square brackets)
-x = ('python','Dart','Java','Dart') #tuple (parenthis)
-x = range(6) # range
-x = {'python','Dart','Java'} #set
+x = range(-455555,221212126) # range
+sasas = ['python','Dart','Java','Dart'] #List ( square brackets) changle order
+x = ('python','Dart','Java','Dart') #tuple (parenthis) cannnot change order
+sasa = {'python','Dart','Dart','Java'} #set
 x =  frozenset({'python','Dart','Java'}) #frozenset
 x = {"name":"Sai Gopi",'age':25} #dict {'key':'value'}
 x = True | False #bool
@@ -17,19 +13,5 @@ x = b'hello' #bytes
 x = bytearray(5) #bytearray
 x = memoryview(bytes(5)) #memory
 
-# specify 
-
-x = str("String world ") # string
-x = int(25) #int
-x = float(25.04) #float
-x = complex(1j) # complex
-x = list('python','Dart','Java','Dart') #List ( square brackets)
-x = tuple('python','Dart','Java','Dart') #tuple (parenthis)
-x = range(6) # range
-x = set('python','Dart','Java') #set
-x =  frozenset({'python','Dart','Java'}) #frozenset
-x = dict(name = "Sai Gopi",age =25) #dict {'key':'value'}
-x = bool(5)  #bool
-x = bytes(b'hello') #bytes
-x = bytearray(5) #bytearray
-x = memoryview(bytes(5)) #memory
+for x in sasas:
+    print(x)
