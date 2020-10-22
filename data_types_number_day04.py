fa2e773d89fe7912bf0efbf420d88803a8c95bcc
saigopi <narimetisaigopi@gmail.com>
2020-10-22 07:11:44 +0530

    Day 4

--- data_types_number_day04.py
+++ data_types_number_day04.py
@@ -1,44 +1,17 @@
-x = 12
-y = 12.00
-z = 1j
+'''
+int
+float
+complex
+'''
+
+priceV = int(-99921212121212121) #int
+price = float(-9990001111111.580000000000000000) #float
+
+value = float(2e+4) #float
+
+#print(value)
+com = complex(1j) #complex
+
+print(com(price))
 
 
-# int
-x = 12333
-x = -12333
-x = -12339999999992132392392392932932939239
-
-#print(type(x))
-
-
-# float (Float point number)
-
-x = 12.80099291291939139193193
-y = -14322.89
-z = 121121212121212.1
-
-#print(type(z))
-
-x = 2e3
-x = -34.4e3
-
-# complex
-
-x = 3+5j
-
-#print(type(x))
-
-#***************** we can convert complex data type to number **********
-
-age = 25
-percentate = 95.67
-z = 1j
-
-x = float(age)
-print(x)
-
-x = int(percentate)
-print(x)
-
-x = complex(percentate)
-print(x)
