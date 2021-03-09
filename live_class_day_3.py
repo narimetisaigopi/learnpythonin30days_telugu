c7c9ff062997e365ec74d9d6194b51fb397e31d0
saigopi <narimetisaigopi@gmail.com>
2021-03-09 07:55:06 +0530

    day 3 added

---
+++ live_class_day_3.py
@@ -0,0 +1,37 @@
+# if else conditional statements
+age = 18
+
+a = 19
+b = 19
+
+'''
+conditional operators
+Results: True/False
+==
+!= 
+>
+<
+>=
+<=
+!
+print(a <= b)
+'''
+
+#nested if else
+# and keywords
+# or
+if age > 18 and age == 18:
+    print("he is adult")
+elif age < 18 or a > 10:
+    print("he is minor")
+else:
+    print("he is started")
+
+if age == 18: print("*********") # shortend operators
+
+if age > 18:
+    print("Eligble for applying vote")
+else:
+    print("Not Eligble for applying vote")
+
+
