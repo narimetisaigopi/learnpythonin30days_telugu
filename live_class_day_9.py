a525de9a81d32fdcde403c8ea271fa547689dc51
saigopi <narimetisaigopi@gmail.com>
2021-03-20 21:17:25 +0530

    day 9 added

---
+++ live_class_day_9.py
@@ -0,0 +1,27 @@
+# Python Modules
+# Module extension .py
+import mymodule
+#import mymodule as my
+#my.helloFromMyModule("Sai Gopi")
+
+from mymodule import mydata
+
+import platform
+import datetime
+
+#print("My Data "+str(mydata))
+
+x = platform.system()
+#print(dir(mymodule))
+
+date = datetime.datetime.now()
+
+print(dir(datetime))
+
+
+
+
+
+
+
+
---
+++ mymodule.py
@@ -0,0 +1,11 @@
+piValue = "3.145490"
+
+def helloFromMyModule(name):
+    print("Saying hello from  Module")
+
+
+
+mydata = {
+    "name" : "Sai Gopi",
+    "age" : 25,
+}
